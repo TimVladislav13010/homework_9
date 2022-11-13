e074294586bf9d70edc99567df26abc7ed8e7420
TimVladislav13010 <vladislav130ua1@gmail.com>
2022-11-13 16:10:16 +0200

    start

---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+
+
+def handler():
+    user_comands = {
+        "hello": hello,
+        "add": add,
+        "change": change,
+        "phone": phone,
+        "show all": show_all,
+        "good bye": good_bye,
+        "close": good_bye,
+        "exit": good_bye,
+        ".": good_bye
+    }
+
+
+def hello():
+    pass
+
+
+def add():
+    pass
+
+
+def change():
+    pass
+
+
+def phone():
+    pass
+
+
+def show_all():
+    pass
+
+
+def good_bye():
+    pass
+
+
+def main():
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    pass
