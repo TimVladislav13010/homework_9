722d408d664ddbd3c616be91d6204d0ffe6b3cd5
TimVladislav13010 <109065019+TimVladislav13010@users.noreply.github.com>
2022-11-14 13:42:15 +0200

    work

--- bot_assistant.py
+++ bot_assistant.py
@@ -1,4 +1,4 @@
-PHONE_BOOK = {"name": "number"}
+PHONE_BOOK = {}
 
 
 """
@@ -7,101 +7,145 @@ PHONE_BOOK = {"name": "number"}
 """
 
 
-def input_error_index(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except IndexError:
-            print(f"Введіть будь ласка команду (hello, add, change, phone, show_all, good_bye, close, exit, .)")
-            main()
-    return wrapper
-
-
-def input_error_type(func):
+def input_error(func):
+    """
+    Обробник помилок
+    """
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except TypeError:
-            print(f"Неправильна команда. Введіть будь ласка команду"
-                  f" (hello, add, change, phone, show_all, good_bye, close, exit, .)")
-            main()
-    return wrapper
-
-
-def input_error_value(func):
-    def wrapper():
-        try:
-            return func()
+            return f"Wrong comand."
+        except KeyError:
+            return f"KeyError"
+        except IndexError:
+            return f"Wrong index"
         except ValueError:
-            print(f"Неправильне введення. Введіть Ім'я та номер через пробіл Ім'я +380981234567")
+            return f"ValueError"
     return wrapper
 
 
+@input_error
 def handler(commands):
-    commands = commands.lower()
-    users_commands = {
-        "hello": hello,
-        "add": add,
-        "change": change,
-        "phone": phone,
-        "show_all": show_all,
-        "good_bye": good_bye,
-        "close": good_bye,
-        "exit": good_bye,
-        ".": good_bye
-    }
-    return users_commands.get(commands)
+    return USER_COMMANDS.get(commands, break_f)
 
 
+def change_input(user_input):
+    """
+    Функція для обробки введених даних від користувача
+    """
+    new_input = user_input.split(" ")
+    return new_input
+    
+    
 def hello():
-    print("How can I help you?")
+    return "How can I help you?"
 
 
-@input_error_value
-def add():
-    phone_number = input("Введіть Ім'я та номер телефону через пробіл: ").split(" ")
+@input_error
+def add(name, number):
+    """
+    Функція для додавання нового номеру в телефонну книгу
+    """
+    if name in PHONE_BOOK:
+        return f"Цей контакт {name} вже використовується введіть інше ім`я"
+    
+    phone_number = (name, number)
     PHONE_BOOK.update([phone_number])
-    print(f"Запис ({phone_number[0]} : {phone_number[1]}) додано до словника")
+    return f"Запис ({name} : {number}) додано до словника"
 
 
-@input_error_value
-def change():
-    name_number = input("Введіть Ім'я та новий номер телефону через пробіл: ").split(" ")
-    if len(name_number) != 2:
-        raise ValueError
-    PHONE_BOOK[name_number[0]] = name_number[1]
-    print(f"Запис ({name_number[0]} : {name_number[1]}) замінено в словнику")
+@input_error
+def change(name, number):
+    """
+    Функціця для змінни існуючого номеру в телефонній книзі
+    """
+    if name not in PHONE_BOOK:
+        return f"{name} імя не знайдено в словнику"
+    
+    elif not number.isdigit():
+        return f"{number} не числово будь ласка введіть числа"
+    
+    PHONE_BOOK[name] = number
+    
+    return f"Запис ({name} : {number}) замінено в словнику"
 
 
-def phone():
-    name_phone = input("Введіть Ім'я користувача: ")
-    phones = PHONE_BOOK.get(name_phone)
-    print(f"{name_phone} : {phones}")
+def phone(name):
+    """
+    Функція повертає номер телефону з телефонної книги
+    """
+    if not PHONE_BOOK.get(name):
+        return f"{name} не знайдено в телефонній книзі"
+    phones = PHONE_BOOK.get(name)
+    return f"{name} : {phones}"
 
 
 def show_all():
+    """
+    Функція для відображення всієї телефонної книги
+    """
+    show_number = ""
     for key, val in PHONE_BOOK.items():
-        print(f"Ім'я: {key}, номер: {val}")
+        show_number += "Ім'я: " + key + ", номер: " + val + "\n"
+    return show_number
 
 
 def good_bye():
-    pass
+    return "Good Bye!"
 
 
-@input_error_index
-@input_error_type
+def break_f():
+    """
+    Коли користувач введе щось інше крім команд повертається строка про неправильний ввід команди.
+    """
+    return f"Wrong enter... "
+
+
+USER_COMMANDS = {
+    "hello": hello,
+    "add": add,
+    "change": change,
+    "phone": phone,
+    "show_all": show_all,
+    "good_bye": good_bye,
+    "close": good_bye,
+    "exit": good_bye,
+    ".": good_bye
+}
+
+
 def main():
+    """
+    Логіка роботи бота помічника
+    """
     while True:
-
-        handler_massage = handler(input("Введіть будь ласка команду "
-                                        "(hello, add, change, phone, show_all, good_bye, close, exit, .)"))
-
-        if handler_massage == good_bye:
-            print(f"Good Bye!")
+        result = None
+        user_input = input("Введіть будь ласка команду "
+                                        "(hello, add, change, phone, show_all, good_bye, close, exit, .)")
+        changes_commands = change_input(user_input)
+        result = handler(changes_commands[0])
+        
+        if result == good_bye:
+            print(result())
             break
+        
+        elif len(changes_commands) == 1 or result == show_all or result == hello:
+            result = handler(changes_commands[0])()
+            print(result)
+            
+        elif len(changes_commands) == 2:
+            result = handler(changes_commands[0])(changes_commands[1])
+            print(result)
+            
+        elif len(changes_commands) == 3:
+            result = handler(changes_commands[0])(changes_commands[1], changes_commands[2])
+            print(result)
 
-        handler_massage()
-
+        elif len(changes_commands) > 3:
+            result = f"({user_input}) - ви ввели невірну команду"
+            print(result)
+        
         continue
 
 
